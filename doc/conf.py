22db368cac5b968ff7f0de247bf004c324e1efd9
Sam Wilson <57262657+SamWilsn@users.noreply.github.com>
2022-10-28 11:37:56 -0400

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -1,31 +1,6 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('../src'))
-
-# -- Project information -----------------------------------------------------
-
 project = "MRE"
 copyright = "2021, Foo"
 author = "Foo"
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-
 
 extensions = [
     "autoapi.extension",
@@ -34,33 +9,15 @@ extensions = [
 autoapi_type = "python"
 autoapi_dirs = ["../mre"]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
 
-# The default language to highlight source code in.
 highlight_language = "python3"
 
-# A boolean that decides whether module names are prepended to all object
-# names (for object types where a "module" of some kind is defined), e.g.
-# for py:function directives.
 add_module_names = False
 
-# This value controls how to represent typehints (PEP 484.)
 autodoc_typehints = "signature"
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "alabaster"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
